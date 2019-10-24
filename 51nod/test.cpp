e0d86660acef958a00cde4b62511e84d65a50a26
17qingyu <591180224@qq.com>
2019-10-24 15:04:46 +0800

    test

---
+++ 51nod/test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 17qingyu
+	> Mail: 
+	> Created Time: Thu 24 Oct 2019 03:02:08 PM CST
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"hello git"<<endl;
+    return 0;
+}
